709acf88d09435bca01d5aa9cd806d5f5f392065
rsayyed591 <rehansayyed591@gmail.com>
2025-03-04 16:35:21 +0530

    Fix : Solved some errors in Notifications.🪖

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Notifications() {
   const navigate = useNavigate();
