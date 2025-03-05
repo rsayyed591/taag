ab92ed4aaa037468f723b8ba8aa20cd3b0c94bcd
rsayyed591 <rehansayyed591@gmail.com>
2025-03-05 23:15:00 +0530

    Style : Added new Styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}", "./public/index.html", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#12766A",
+        secondary: "#f3f4f6",
+      },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
