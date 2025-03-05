149d293b41f82963c33e0fb9fe9856d0897e1893
rsayyed591 <rehansayyed591@gmail.com>
2025-03-05 06:25:28 +0530

    Style : Change the alignment and have responsiveness of the UserType page.

--- src/pages/UserType.jsx
+++ src/pages/UserType.jsx
@@ -14,41 +14,39 @@ function UserType() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col px-6 pt-6">
+    <div className="page-container">
       {/* Content */}
-      <div className="flex-1 flex flex-col">
-        <h2 className="mb-2 text-2xl leading-[32.78px] font-manrope font-medium">Select your preferred <br/> user type</h2>
-        <p className="text-[15px] leading-[20.49px] font-manrope font-medium text-[#979797] mb-8">Select the option defining your role</p>
+      <div className="content-container pt-6 md:pt-12">
+        <h2 className="mb-2 text-2xl leading-[32.78px] font-manrope font-medium w-[251px] md:w-full md:text-center">Select your preferred user type</h2>
+        <p className="text-[15px] leading-[20.49px] font-manrope font-medium text-[#979797] md:text-center mb-8 md:mt-8">Select the option defining your role</p>
 
         {/* User Type Options */}
-        <div className="flex flex-wrap gap-x-2 gap-y-4 place-items-center mb-8">
-            <div
-                className={`option-card ${selectedType === "Agency" ? "selected" : ""}`}
-                onClick={() => setSelectedType("Agency")}
-            >
-                Agency
-            </div>
+        <div className="flex flex-wrap gap-x-2 gap-y-4 place-items-center md:justify-center md:align-center mb-8">
+          <div
+            className={`option-card ${selectedType === "Agency" ? "selected" : ""}`}
+            onClick={() => setSelectedType("Agency")}
+          >
+            Agency
+          </div>
 
-            <div
-                className={`option-card ${selectedType === "Brand" ? "selected" : ""}`}
-                onClick={() => setSelectedType("Brand")}
-            >
-                Brand
-            </div>
+          <div
+            className={`option-card ${selectedType === "Brand" ? "selected" : ""}`}
+            onClick={() => setSelectedType("Brand")}
+          >
+            Brand
+          </div>
 
-            <div
-                className={`option-card ${selectedType === "Creator" ? "selected" : ""}`}
-                onClick={() => setSelectedType("Creator")}
-            >
-                Creator
-            </div>
+          <div
+            className={`option-card ${selectedType === "Creator" ? "selected" : ""}`}
+            onClick={() => setSelectedType("Creator")}
+          >
+            Creator
+          </div>
         </div>
 
-
-
         {/* Continue Button */}
-        <div className="mt-auto flex justify-center">
-          <button className="btn-primary mb-12" onClick={handleContinue} disabled={!selectedType}>
+        <div className="mt-auto flex justify-center mb-12">
+          <button className="btn-primary2" onClick={handleContinue} disabled={!selectedType}>
             Continue
           </button>
         </div>
