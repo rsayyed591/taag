09dd3bea8fa89e514a0fd0e1d49445aede5b6cae
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2025-03-11 23:51:15 +0530

    buid error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['uuid'],
+    },
+  },
+  optimizeDeps: {
+    include: ['uuid'],
+  },
 })
